90c73b5372849e421a935cc3ea6c6a488844ee5b
kuropatkin <ko-lyambda@mail.ru>
2016-10-18 13:06:22 +0300

    new commit

---
+++ build/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+# Programs that are runnable.
+ns3_runnable_programs = ['build/scratch/ns3.25-scratch-simulator-debug', 'build/scratch/subdir/ns3.25-subdir-debug', 'build/scratch/ns3.25-first-debug']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+
